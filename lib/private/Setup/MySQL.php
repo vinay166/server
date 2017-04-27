owncloud/core

MySQL 4 byte is now detected during setup in any case. config.sample.php was updated to explicitly state that there detection in place to set mysql.utf8mb4

lib/private/Setup/MySQL.php
@@ -40,12 +40,10 @@ class MySQL extends AbstractDatabase {
 		$connection = $this->connect();
 
 		// detect mb4
-		if (is_null($this->config->getSystemValue('mysql.utf8mb4', null))) {
-			$tools = new MySqlTools();
-			if ($tools->supports4ByteCharset($connection)) {
-				$this->config->setSystemValue('mysql.utf8mb4', true);
-				$connection = $this->connect();
-			}
+		$tools = new MySqlTools();
+		if ($tools->supports4ByteCharset($connection)) {
+			$this->config->setSystemValue('mysql.utf8mb4', true);
+			$connection = $this->connect();
 		}
 
 		$this->createSpecificUser($username, $connection);

vinay166/server

lib/private/Setup/MySQL.php
@@ -38,12 +38,10 @@ class MySQL extends AbstractDatabase {
 		$connection = $this->connect(['dbname' => null]);
 
 		// detect mb4
-		if (is_null($this->config->getValue('mysql.utf8mb4', null))) {
-			$tools = new MySqlTools();
-			if ($tools->supports4ByteCharset($connection)) {
-				$this->config->setValue('mysql.utf8mb4', true);
-				$connection = $this->connect();
-			}
+		$tools = new MySqlTools();
+		if ($tools->supports4ByteCharset($connection)) {
+			$this->config->setValue('mysql.utf8mb4', true);
+			$connection = $this->connect();
 		}
 
 		$this->createSpecificUser($username, $connection);